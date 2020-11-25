c9eaafcc8405931eda157f72b1af25bd2cd6be71
john <john@jpatmore.plus.com>
2020-11-25 18:52:20 +0000

    test code added in test.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from turtle import Turtle, Screen
 from random import randint
-
+import time
 
 # Screen setup
 s = Screen()
@@ -18,19 +18,59 @@ snake_body = []
 
 def new_seg(index):
     # Create a snake segment
-    snake_body.append({"seg": Turtle(shape="square"), "pos": (-index * 20, 0)})
+    color = (randint(0, 255), randint(0, 255), randint(0, 255))
+    snake_body.append({"seg": Turtle(shape="square"), "x": -index * 20, "y": 0})
     snake_body[index]["seg"].speed(1)
-    snake_body[index]["seg"].color("white")
-    snake_body[index]["seg"].setposition(snake_body[index]["pos"])
+    snake_body[index]["seg"].color(color)
+    snake_body[index]["seg"].pu()
+    pos = (snake_body[index]["x"], snake_body[index]["y"])
+    snake_body[index]["seg"].setposition(pos)
 
 
+def move_snake(head, tail, dir_x, dir_y):
+    """To move the snake:
+        Make the Tail the new Head with new location depending on direction of movement.
+        All other segments remain untouched.
+    Requires: index to Head, index to Tail, x axis direction, y axis direction
+    Direction x/y should be an integer representing the direction along the axis:
+        1: positive, 0: no movement, -1: negative"""
+    snake_body[tail]["x"] = snake_body[head]["x"] + (20 * dir_x)
+    snake_body[tail]["y"] = snake_body[head]["y"] + (20 * dir_y)
+    pos = (snake_body[tail]["x"], snake_body[tail]["y"])
+    snake_body[tail]["seg"].setposition(pos)
+    new_head = tail
+
+
+
+    # THIS IS IT - returns the INDEX to the first lowest item found:
+    min_x = min(range(len(snake_body)), key=lambda index: snake_body[index]['x'])
+    min_y = min(range(len(snake_body)), key=lambda index: snake_body[index]['y'])
+    print(snake_body)
+    print(min_x, min_y)
+
+
+    # if dir_x == 1:
+    #     tail = min(seq_x)
+    # new_tail =
+
+
+# Create 3 segment snake
 for i in range(0, 3):
     new_seg(i)
 
-# To move the snake:
-#   insert the last tail segment as the new head at the first segment and with a new position
-#   then remove the last segment
-#   All other segments remain untouched
+time.sleep(2)
+
+# Initialize snake
+head_index = 0
+tail_index = 2
+direction_x = 1
+direction_y = 0
+
+# Move snake
+move_snake(head=head_index,
+           tail=tail_index,
+           dir_x=direction_x,
+           dir_y=direction_y)
 
 
 s.exitonclick()
---
+++ test.py
@@ -0,0 +1,55 @@
+# Stuff I've learned
+# ==================
+
+from turtle import Turtle
+
+# The Problem:
+#   I want to find the index of the item
+#   with the minimum values of x and y
+#   in the example list of dictionaries
+
+# Example of list of dictionaries
+snake_body = [
+    {'seg': Turtle(), 'x': 0, 'y': 10},
+    {'seg': Turtle(), 'x': -20, 'y': 0},
+    {'seg': Turtle(), 'x': 20, 'y': -20},
+    {'seg': Turtle(), 'x': -20, 'y': -20},
+]
+
+
+def min_value():
+    # Not quite what I'm after. This only returns the (first) minimum values found.
+    """Make a list out of x and y positions,
+    then find minimum of list"""
+    list_x = [a["x"] for a in snake_body]
+    list_y = [a["y"] for a in snake_body]
+    min_x = min(list_x)
+    min_y = min(list_y)
+    print("Results from min_value()")
+    print(f"list_x = {list_x}, list_y = {list_y}")
+    print(f"min_x = {min_x}, min_y = {min_y}")
+    print()
+
+
+def min_item():
+    # Better. This returns the entire item of the (first) lowest item found.
+    min_x = min(snake_body, key=lambda x: x["x"])
+    min_y = min(snake_body, key=lambda y: y["y"])
+    print("Results from min_item()")
+    print(f"min_x = {min_x}, min_y = {min_y}")
+    print()
+
+
+def min_index():
+    # This is what I need - returns the INDEX to the (first) lowest item found.
+    # I'm still not quite sure how it works though!
+    min_x = min(range(len(snake_body)), key=lambda index: snake_body[index]['x'])
+    min_y = min(range(len(snake_body)), key=lambda index: snake_body[index]['y'])
+    print("Results from min_index()")
+    print(f"min_x = {min_x}, min_y = {min_y}")
+    print()
+
+
+min_value()
+min_item()
+min_index()
