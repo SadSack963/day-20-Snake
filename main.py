f61351be7e41238ff8c35ade0c3c372271dc1988
john <john@jpatmore.plus.com>
2020-11-25 01:58:57 +0000

    Initial code

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+from random import randint
+
+
+# Screen setup
+s = Screen()
+s.colormode(255)
+s.bgcolor(0, 0, 0)
+
+# Turtle setup
+t = Turtle()
+t.speed(1)
+t.color("white")
+
+
+s.exitonclick()
