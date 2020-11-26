6d24403dd153cabf203668c2d6e708af1bafe4de
john <john@jpatmore.plus.com>
2020-11-26 21:35:30 +0000

    My attempt aborted - too complicated to find out which segment is the Tail. Now proceeding with code based on Angela's approach. snake_body[] is now a simple list of turtles.

--- angela.py
+++ angela.py
@@ -8,43 +8,41 @@ s.colormode(255)
 s.bgcolor("black")
 s.setup(width=600, height=600)
 s.title("Snake")
+s.tracer(0)  # Turn off screen animation
+
 
 # Turtle setup
-
-# List of snake segments: [{turtle, x position, y position}]
+# List of snake segments: [Turtle()]
 snake_body = []
 seg_size = 20
 
 
 def new_seg(x, y):
-    color = (randint(0, 255), randint(0, 255), randint(0, 255))
     new_segment = Turtle(shape="square")
     new_segment.speed(1)
-    new_segment.color(color)
+    new_segment.color("white")
     new_segment.pu()
-    pos = (x, y)
-    new_segment.setposition(pos)
-    snake_body.append({"seg": new_segment, "x": x, "y": y})
+    new_segment.setposition(x, y)
+    snake_body.append(new_segment)
 
 
 def move_snake(index):
-    new_pos_x = snake_body[index-1]["x"]
-    new_pos_y = snake_body[index-1]["y"]
-    snake_body[index]["x"] = new_pos_x
-    snake_body[index]["y"] = new_pos_y
-    snake_body[index]["seg"].goto(new_pos_x, new_pos_y)
+    new_pos_x = snake_body[index-1].xcor()  # get x coordinate
+    new_pos_y = snake_body[index-1].ycor()  # get y coordinate
+    snake_body[index].goto(new_pos_x, new_pos_y)
 
 
+# Create 3 segment snake
 for i in range(0, -3, -1):
     new_seg(x=(i * seg_size), y=0)
+    s.update()
 
-
-while snake_body[0]["x"] < 280:
+while snake_body[0].xcor() < 280:
     for i in range(len(snake_body) - 1, 0, -1):
         move_snake(i)
-    snake_body[0]["seg"].fd(seg_size)
-    snake_body[0]["x"] += seg_size
-    snake_body[0]["y"] = 0
+    snake_body[0].fd(seg_size)
+    s.update()
+    time.sleep(0.5)  # slow down the animation
 
 #     time.sleep(1)  # gives time to view the colours
 
